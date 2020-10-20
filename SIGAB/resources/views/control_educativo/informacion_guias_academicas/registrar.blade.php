a5ab9c9e987259466f14fdf6058e9e95c7d9f3c8
Ivan25e <57422560+Ivan25e@users.noreply.github.com>
2020-10-20 09:47:49 -0600

    Bug guía académica

--- SIGAB/resources/views/control_educativo/informacion_guias_academicas/registrar.blade.php
+++ SIGAB/resources/views/control_educativo/informacion_guias_academicas/registrar.blade.php
@@ -194,12 +194,7 @@ Registrar guía académica
 
 <script src="{{ asset('js/global/contarCaracteres.js') }}" defer></script>
 <script>
-    let est = {
-        {
-            $estudiante - > persona - > persona_id
-        }
-    }
-
+    let est = {{$estudiante->persona->persona_id}}
 </script>
 <script src="{{ asset('js/control_educativo/informacion_guias_academicas/registrar.js') }}" defer></script>
 @endsection
