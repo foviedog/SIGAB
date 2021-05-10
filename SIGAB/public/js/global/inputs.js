3391d2044227aa26b53792dd94c2eadfe93eb602
Iván Chinchilla <57422560+Ivan25e@users.noreply.github.com>
2021-05-09 18:46:31 -0600

    Arreglo de datepickers

--- SIGAB/public/js/global/inputs.js
+++ SIGAB/public/js/global/inputs.js
@@ -7,6 +7,15 @@ $(function() {
         locale: {
             format: "DD/MM/YYYY",
             cancelLabel: "Clear"
-        }
+        },
+        autoUpdateInput: false
+    });
+
+    $('.datetimepicker').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+    });
+
+    $('.datetimepicker').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
     });
 });
