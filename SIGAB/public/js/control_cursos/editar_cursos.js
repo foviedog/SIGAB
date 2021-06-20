ac456b6fd3b653d57b5b71938536335663541b9c
David <david0406p2009@hotmail.com>
2021-06-19 22:28:41 -0600

    detalle

--- SIGAB/public/js/control_cursos/editar_cursos.js
+++ SIGAB/public/js/control_cursos/editar_cursos.js
@@ -1,34 +1,31 @@
-$("#cancelar-edi").hide();
-$("#guardar-cambios").hide();
-$("#cambiar-foto").hide();
-
-$("#editar-estudiante").on("click", function() {
-    $("input").removeAttr("disabled");
-    $("select").removeAttr("disabled");
-    $("textarea").removeAttr("disabled");
-    $("#editar-estudiante").hide();
-    $("#cancelar-edi").show();
-    $("#guardar-cambios").show();
-    $("#cambiar-foto").show();
-});
-
-$("#cancelar-edi").on("click", function () {
-    location.reload(); // Recarga la página inicial para eliminar todos los cambios hechos y volver a bloquer todos los cambios
-});
+document.addEventListener("DOMContentLoaded", cargaInicial);//Se agrega el evento carga inicial al momento de cargar el documento
+// ===============================================================================================
+//Función encargada de hacer elllamado  de todos los métodos utilizados en el registro.
+// ===============================================================================================
+function cargaInicial(event) {
+    habilitarEdicion();
+    cancelarEdicion();
+}
 
 // =======================================================================
 // Evento que habilita el botón si se está de acuerdo con las condiciones
 // =======================================================================
-
-$(function () {
-    $('.check-list').on('click', function () {
-        if($('#checkTrabajos:checked').length > 0 
-            && $('#checkTitulaciones:checked').length > 0 
-            && $('#checkGuias:checked').length > 0
-            && $('#checkListas:checked').length > 0){
-            $("#button-submit-eliminar").prop("disabled", false);
-        } else {
-            $("#button-submit-eliminar").prop("disabled", true);
-        }
+function habilitarEdicion() {
+    $('#editar').on('click', function () {
+        $("input").removeAttr("disabled");
+        $('#editar').hide()
+        $('#cancelar-edit').show()
     });
-});
+}
+
+// =================================================================
+// Función que se encarga de ocultar elementos al cargar la página
+// =================================================================
+function cancelarEdicion() {
+    $('#cancelar-edit').on('click', function () {
+        location.reload(); // Recarga la página inicial para eliminar todos los cambios hechos y volver a bloquer todos los cambios
+    });
+}
+
+
+
--- SIGAB/resources/views/control_cursos/detalle.blade.php
+++ SIGAB/resources/views/control_cursos/detalle.blade.php
@@ -13,14 +13,15 @@ Registrar curso
     <div class="card-body pb-5">
         <div class="d-flex justify-content-between">
             <h2>Detalle curso</h2>
-            <div>
+            <div class="d-flex ">
                 @if(Accesos::ACCESO_LISTAR_ESTUDIANTES())
                 <div><a href="{{ route('listado-estudiantil') }}" class="btn btn-contorno-rojo"><i class="fas fa-chevron-left "></i> &nbsp; Volver al listado </a></div>
+                <div><button class="btn btn-rojo ml-1" id="editar"><i class="fas fa-edit"></i> &nbsp; Editar </button></div>
+                <div><button class="btn btn-rojo ml-1" id="cancelar-edit" style="display: none;"><i class="fas fa-times"></i> &nbsp; Cancelar </button></div>
                 @endif
             </div>
         </div>
         <hr>
-
         @if(Accesos::ACCESO_REGISTRAR_ESTUDIANTES())
         {{-- Formulario para registrar informacion del estudiante --}}
         <form action="{{ route('estudiante.store') }}" autocomplete="off" method="POST" enctype="multipart/form-data" id="estudiante" onsubmit="activarLoader('Agregando Estudiante');">
@@ -30,29 +31,35 @@ Registrar curso
             @include('layouts.messages.alerts')
 
             {{-- Mensaje de que muestra el objeto insertado (solo se muestra si ha sido exitoso el registro)  --}}
-            @if(isset($estudiante_insertado))
+            @if(isset($curso_insertado))
             <div class="alert alert-dark" role="alert">
-                Se registró el estudiante con lo siguientes datos: <br> <br>
+                Se registró el curso con lo siguientes datos: <br> <br>
                 <div class="row">
                     <div class="col-6 text-justify">
-                        {{-- <b>Cédula:</b> {{ $persona_insertada->persona_id }} <br>
-                        <b>Nombre/s:</b> {{ $persona_insertada->nombre }} <br>
-                        <b>Apellido/s:</b> {{ $persona_insertada->apellido }} <br>
-                        <b>Cantidad de hijos:</b> {{ $estudiante_insertado->cant_hijos ?? "No se digitó" }} <br> --}}
+                        <b>Código:</b> {{ $curso_insertado->codigo ?? "No se digitó" }} <br>
+                        <b>Nombre:</b> {{ $curso_insertado->nombre ?? "No se digitó" }} <br>
+                        <b>NRC:</b> {{ $curso_insertado->nombre ?? "No se digitó" }} <br>
                         {{-- Link directo al estudiante recien agregado --}}
-                        {{-- <br>
-                    <a clas="btn btn-rojo" href="{{ route('estudiante.show', $estudiante_insertado->persona_id) }}">
-                        <input type="button" @if(Accesos::ACCESO_MODIFICAR_ESTUDIANTES()) value="Editar" @else value="Detalle" @endif class="btn btn-rojo">
+                        <br>
+                        <a clas="btn btn-rojo" href="{{ route('cursos.show', $curso_insertado->codigo) }}">
+                        
+                        <input type="button" 
+                        {{-- @if(Accesos::ACCESO_MODIFICAR_ESTUDIANTES()) --}}
+                            value="Editar"
+                        {{-- @else value="Detalle" --}}
+                        {{-- @endif  --}}
+                        class="btn btn-rojo">
+
                         </a>
-                        <br> --}}
+                        <br>
                     </div>
                 </div>
-                <div class="h3 mb-5 mt-4 mx-3">Agregar un nuevo curso:</div>
-                @endif
+            </div>
+            <div class="h3 mb-5 mt-4 mx-3">Agregar un nuevo curso:</div>
+            @endif
                 {{-- INICIO DE LA SECCION DE REGISTRO --}}
                 <div class="row d-flex justify-content-center align-items-center">
                     <div class="col-9">
-
                         <div class="card">
                             <div class="card-header">
                                 <p class="texto-rojo-medio m-0 font-weight-bold texto-rojo">Información general </p>
@@ -65,7 +72,7 @@ Registrar curso
                                     <div class="col-9 d-flex flex-column">
                                         <div class="d-flex justify-content-center w-100">
                                             <div class="input-group px-2 mb-3 w-100 ml-1 pl-3">
-                                                <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre del curso"  required disabled>
+                                                <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre del curso" value="{{ $curso->nombre }}"  required disabled>
                                                 <div class="input-group-append">
                                                     <span class="input-group-text" data-toggle="tooltip" data-placement="top" title="Nombre con el que quiere guardar el curso"><i class="far fa-edit texto-azul-una"></i></span>
                                                 </div>
@@ -74,16 +81,16 @@ Registrar curso
                                         </div>
                                         <div class="d-flex justify-content-center w-100">
                                             <div class="input-group px-2 mb-3 w-75 ml-1 pl-3">
-                                                <input type="text" id="codigo" name="codigo" class="form-control" placeholder="Código de curso" onkeydown="contarCaracteres(this,15)" required disabled>
+                                                <input type="text" id="codigo" name="codigo" class="form-control" placeholder="Código de curso" value="{{ $curso->codigo }}"  onkeydown="contarCaracteres(this,15)" required disabled>
                                                 <div class="input-group-append">
-                                                    <span class="input-group-text" data-toggle="tooltip" data-placement="top" title="Código de curso"><i class="far fa-edit texto-azul-una"></i></span>
+                                                    <span class="input-group-text" data-toggle="tooltip" data-placement="top" title="Código de curso"><i class="far fa-question-circle fa-1x texto-azul-una"></i></span>
                                                 </div>
                                                 <span class="ml-1 text-muted d-flex align-items-center" id="mostrar_codigo"></span>
                                             </div>
                                             <div class="input-group px-2 mb-3 w-75 ml-1 pl-3">
-                                                <input type="text" id="nrc" name="nrc" aria-label="NRC" class="form-control" placeholder="NRC"  required disabled>
+                                                <input type="text" id="nrc" name="nrc" aria-label="NRC" class="form-control" placeholder="NRC" value="{{ $curso->nrc }}" required disabled>
                                                 <div class="input-group-append">
-                                                    <span class="input-group-text" data-toggle="tooltip" data-placement="top" title="NRC del curso"><i class="far fa-edit texto-azul-una"></i></span>
+                                                    <span class="input-group-text" data-toggle="tooltip" data-placement="top" title="NRC del curso"><i class="far fa-question-circle fa-1x texto-azul-una"></i></span>
                                                 </div>
                                                 <span class="ml-1 text-muted d-flex align-items-center" id="mostrar_nrc"></span>
                                             </div>
@@ -105,4 +112,5 @@ Registrar curso
     @section('scripts')
     <script src="{{ asset('js/global/mensajes.js') }}"></script>
     <script src="{{ asset('js/global/contarCaracteres.js') }}"></script>
+    <script src="{{ asset('js/control_cursos/editar_cursos.js') }}"></script>
     @endsection
