2e5d02312d9e6ccf8dad219fb3de7501e3f8b8ff
Iván Chinchilla <57422560+Ivan25e@users.noreply.github.com>
2021-06-21 14:38:20 -0600

    Descomentado try catch

--- SIGAB/app/Http/Controllers/CursoController.php
+++ SIGAB/app/Http/Controllers/CursoController.php
@@ -43,7 +43,7 @@ class CursoController extends Controller
     }
 
     public function store(Request $request){
-        //try{
+        try{
 
             $curso = new Curso; // Creación de una nueva instacia Curso
 
@@ -64,13 +64,13 @@ class CursoController extends Controller
             ->with('mensaje_exito', "¡El curso se ha registrado exitosamente!") //Mensaje de éxito
             ->with('curso_insertado', $curso); //Retorna el objeto que se insertó
         
-        /*} catch (\Illuminate\Database\QueryException $ex) { 
+        } catch (\Illuminate\Database\QueryException $ex) { 
             //Manejo de posibles errores 
             return view('control_cursos.registrar')
                 ->with('mensaje_error', "Ha ocurrido un error con el registro del curso con el código " . "$request->codigo" . ". Es posible que el curso ya se encuentre agregado.");
         } catch (\Exception $exception) {
             throw new ControllerFailedException();
-        }*/
+        }
     }
 
     public function update($codigo, Request $request){
