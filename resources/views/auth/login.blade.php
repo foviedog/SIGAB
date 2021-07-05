eebf09b9cdbbccba9c134c0e5e1b24a77a804379
David <david0406p2009@hotmail.com>
2021-07-05 11:47:03 -0600

    Prueba2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                 <div class="card shadow-lg o-hidden border-0 my-5">
 
                     <div class="card-header bg-rojo-oscuro">
-                        <div class="text-lext font-weight-bold text-white bg-azul-una">
+                        <div class="text-lext font-weight-bold text-white bg-rojo-oscuro">
                             Inicio de sesión
                         </div>
                     </div>
